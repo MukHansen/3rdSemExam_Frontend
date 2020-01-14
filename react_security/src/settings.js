d5f045df226e5997b6e7cf9280681bb4b2a5e8f4
Mads <cph-mh752@cphbusiness.dk>
2020-01-14 05:06:46 +0100

    Url settings changed

--- react_security/src/settings.js
+++ react_security/src/settings.js
@@ -1,5 +1,5 @@
-// const URL = "https://mukh.dk/3rdSemExam";
+const URL = "https://mukh.dk/3rdSemExam";
 
-const URL = "http://localhost:8080/3rdSemExam";
+// const URL = "http://localhost:8080/3rdSemExam";
 
 export default URL;
